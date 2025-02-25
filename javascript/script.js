cc7fec4db9b827d5d142c97fcb9dfb3fba4a29b0
VanesssaDndll <vanessadonadelli2014@hotmail.com>
2025-02-25 18:09:57 -0300

    variables and operators - JavaScript

---
+++ javascript/script.js
@@ -0,0 +1,57 @@
+let name = "Josh";
+let surname = "Doe";
+
+console.log(name);
+console.log(surname);
+
+// ----------------------------------------------------------
+
+let age = 11;
+console.log(age);
+
+age = 44;
+console.log(age);
+
+// ----------------------------------------------------------
+
+const pi = 3.14;
+
+// pi = 10;
+
+console.log(pi);
+
+// ----------------------------------------------------------
+
+console.log(( 3 + 2 ) - 76 * (1 + 1));
+
+// ---------------------- TAREFAS ---------------------------
+
+console.log(23 + 97);
+
+console.log(32 + 7 - 9 + 90 - 130 + 21);
+
+console.log((4 + 6 + 9) / 77);
+
+let a = 10;
+console.log(a);
+
+a = 22;
+console.log(a);
+
+let b = 7 * a;
+console.log(b);
+
+const max = 57;
+const actual = max - 13;
+const percentage = actual / max;
+console.log(percentage);
+
+// ----------------------------------------------------------
+
+let esolha = "A";
+
+if (esolha != "A"){
+    console.log("Letra errada!");
+} else {
+    console.log("Acertou a letra!");  
+}
