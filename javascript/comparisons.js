6a905e32ab8ac282f927cda1e2a48295a395710a
VanesssaDndll <vanessadonadelli2014@hotmail.com>
2025-05-21 15:41:00 -0300

    comparisons in JavaScript

---
+++ javascript/comparisons.js
@@ -0,0 +1,79 @@
+// ----------------------------- BOOLEAN ----------------------------
+console.log("BOOLEAN");
+
+console.log(2 > 1);
+console.log(2 == 1);
+console.log(2 != 1);
+
+let result = 5 > 4;
+console.log(result);
+
+// ------------------------------------------------------------------
+
+// ----------------------------- STRING -----------------------------
+console.log("\nSTRING");
+
+console.log('Z' > 'A');
+console.log('Glow' > 'Glee');
+console.log('Bee' > 'Be');
+
+// ------------------------------------------------------------------
+
+// ----------------------------- DIFFERENT TYPES --------------------
+console.log("\nDIFFERENT TYPES");
+
+console.log('2' > 1);
+console.log('1' == 1);
+
+console.log(true == 1);
+console.log(false == 0);
+
+// ------------------------------------------------------------------
+
+// ----------------------------- STRICT EQUALITY --------------------
+console.log("\nSTRICT EQUALITY");
+
+console.log(0 == false);
+console.log('' == false);
+
+console.log(0 === false);
+
+// ------------------------------------------------------------------
+
+// ----------------------------- NULL UNDEFINED ---------------------
+console.log("\nNULL UNDEFINED");
+
+console.log(null === undefined);
+console.log(null == undefined);
+
+// ------------------------------------------------------------------
+
+// ----------------------------- NULL x 0 ---------------------------
+console.log("\nNULL x 0");
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+// ------------------------------------------------------------------
+
+// ----------------------------- UNDEFINED --------------------------
+console.log("\nUNDEFINED");
+
+console.log(undefined > 0);
+console.log(undefined < 0);
+console.log(undefined == 0);
+
+// ------------------------------------------------------------------
+
+// ----------------------------- TASK -------------------------------
+console.log("\nTASK");
+
+console.log(5 > 4);                 // true
+console.log("apple" > "pineapple"); // false
+console.log(undefined == null);     // true
+console.log(undefined === null);    // false
+console.log(null == "\n0\n");       // false
+console.log(null === "\n0\n");      // false
+
+// ------------------------------------------------------------------
